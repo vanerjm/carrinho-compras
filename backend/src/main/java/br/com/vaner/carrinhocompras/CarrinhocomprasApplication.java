3b7bbb2fd253e252ecd283f4eaa4ee662192aa71
Vaner Magalhaes <vaner@inatel.br>
2020-02-21 00:14:11 -0300

    Official Release

--- backend/src/main/java/br/com/vaner/carrinhocompras/CarrinhocomprasApplication.java
+++ backend/src/main/java/br/com/vaner/carrinhocompras/CarrinhocomprasApplication.java
@@ -1,15 +1,20 @@
 package br.com.vaner.carrinhocompras;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarrinhocomprasApplication {
+public class CarrinhocomprasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarrinhocomprasApplication.class, args);
-	}
+	}	
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CarrinhocomprasApplication.class);
+    }
 }
 	
